79931e0780d955879b1856eecbeaae9521e5c7c0
ClintWalsh <clintwalsh28@gmail.com>
2022-07-14 19:42:36 -0700

    finished step 2

--- tut_01/lib/main.dart
+++ tut_01/lib/main.dart
@@ -62,13 +62,33 @@ class _SignUpFormState extends State<SignUpForm> {
 
   double _formProgress = 0;
 
-void _showWelcomeScreen() {
-  Navigator.of(context).pushNamed('/welcome');
-}
+  void _showWelcomeScreen() {
+    Navigator.of(context).pushNamed('/welcome');
+  }
+
+  void _updateFormProgress() {
+    var progress = 0.0;
+    final controllers = [
+      _firstNameTextController,
+      _lastNameTextController,
+      _usernameTextController
+    ];
+
+    for (final controller in controllers) {
+      if (controller.value.text.isNotEmpty) {
+        progress += 1 / controllers.length;
+      }
+    }
+
+    setState(() {
+      _formProgress = progress;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      onChanged: _updateFormProgress,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -110,7 +130,8 @@ void _showWelcomeScreen() {
                     : Colors.blue;
               }),
             ),
-            onPressed: _showWelcomeScreen,
+            onPressed:
+                _formProgress == 1 ? _showWelcomeScreen : null, // UPDATED
             child: const Text('Sign up'),
           ),
         ],
